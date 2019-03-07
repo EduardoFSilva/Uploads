85d09eafab2ba4082517b54aaccce09c67f0337c
EduardoFSilva <40077011+EduardoFSilva@users.noreply.github.com>
2019-03-07 15:24:29 -0300

    Add files via upload

---
+++ logos.sh
@@ -0,0 +1,70 @@
+#distros=ALDOS, Alpine Linux, Amazon Linux, Antergos, Arch Linux (Old and Current Logos), Artix Linux, \
+#blackPanther OS, BLAG, BunsenLabs, CentOS, Chakra, Chapeau, Chrome OS, Chromium OS, CrunchBang, CRUX, \
+#Debian, Deepin, DesaOS,Devuan, Dragora, elementary OS, Evolve OS, Exherbo, Fedora, Frugalware, Fuduntu, Funtoo, \
+#Fux, Gentoo, gNewSense, GuixSD, Hyperbola GNU/Linux-libre, januslinux, Jiyuu Linux, Kali Linux, KaOS, KDE neon, Kogaion, Korora, \
+#LinuxDeepin, Linux Mint, LMDE, Logos, Mageia, Mandriva/Mandrake, Manjaro, Mer, Netrunner, NixOS, OBRevenge, openSUSE, \
+#OS Elbrus, Oracle Linux, Parabola GNU/Linux-libre, Pardus, Parrot Security, PCLinuxOS, PeppermintOS, Proxmox VE, PureOS, Qubes OS, \
+#Raspbian, Red Hat Enterprise Linux, ROSA, Sabayon, SailfishOS, Scientific Linux, Siduction, Slackware, Solus, Source Mage GNU/Linux, \
+#SparkyLinux, SteamOS, SUSE Linux Enterprise, SwagArch, TinyCore, Trisquel, Ubuntu, Viperr, Void and Zorin OS
+
+
+clear
+ 
+
+echo  
+echo  
+echo  
+echo 
+screenfetch -L -A 'Alpine Linux'
+screenfetch -L -A 'Antergos'
+screenfetch -L -A 'Arch Linux'
+screenfetch -L -A 'BLAG'
+screenfetch -L -A 'BunsenLabs'
+screenfetch -L -A 'CentOS'
+screenfetch -L -A 'Chakra'
+screenfetch -L -A 'Chapeau'
+screenfetch -L -A 'CrunchBang'
+screenfetch -L -A 'CRUX'
+screenfetch -L -A 'Debian'
+screenfetch -L -A 'Deepin'
+screenfetch -L -A 'Devuan'
+screenfetch -L -A 'elementary OS'
+screenfetch -L -A 'Exherbo'
+screenfetch -L -A 'Fedora'
+screenfetch -L -A 'Frugalware'
+screenfetch -L -A 'Fuduntu'
+screenfetch -L -A 'Funtoo'
+screenfetch -L -A 'Fux'
+screenfetch -L -A 'Gentoo'
+screenfetch -L -A 'gNewSense'
+screenfetch -L -A 'Jiyuu Linux'
+screenfetch -L -A 'Kali Linux'
+screenfetch -L -A 'KaOS'
+screenfetch -L -A 'KDE neon'
+screenfetch -L -A 'Kogaion'
+screenfetch -L -A 'Korora'
+screenfetch -L -A 'LinuxDeepin'
+screenfetch -L -A 'Mint'
+screenfetch -L -A 'LMDE'
+screenfetch -L -A 'Logos'
+screenfetch -L -A 'Mageia'
+screenfetch -L -A 'Mandrake'
+screenfetch -L -A 'Manjaro'
+screenfetch -L -A 'Netrunner'
+screenfetch -L -A 'NixOS'
+screenfetch -L -A ''
+screenfetch -L -A ''
+screenfetch -L -A ''
+screenfetch -L -A ''
+screenfetch -L -A ''
+screenfetch -L -A ''
+screenfetch -L -A ''
+screenfetch -L -A ''
+echo  
+echo  
+echo  
+echo 
+
+
+
+
